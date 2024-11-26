23255100f14f7ac76de3ca77d7e8222c27518959
Umair Zubair <umair.zubair@tkxel.io>
2024-11-26 11:25:18 +0500

    uzu-001-user-api-rate-limit-initial-commit

---
+++ src/main/java/com/uzu/user/UserApiRatelimitCacheApplication.java
@@ -0,0 +1,13 @@
+package com.uzu.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApiRatelimitCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApiRatelimitCacheApplication.class, args);
+	}
+
+}
